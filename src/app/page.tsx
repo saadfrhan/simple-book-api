de2296349d4228a4ba691cf9564f5b02d9566fcd
saadfrhan <saadfarhan347@gmail.com>
2023-04-16 06:37:47 +0500

    feat: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>Hello World!</h1>
+  return <h1
+    style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}
+  >Hello World!</h1>
 }
--- src/utils/getAuthorizationHeader.ts
+++
@@ -1,8 +0,0 @@
-
-export function getAuthorizationHeader() {
-  const token = localStorage.getItem("token");
-
-  return {
-    Authorization: `Bearer ${JSON.parse(token || "")?.accessToken || ""}`,
-  };
-}
